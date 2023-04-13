eb78f8a514d1f81dc2b05fb3d14e53d5be21649c
Aner Lucero <4rgento@gmail.com>
2023-04-13 00:21:15 -0300

    Export library function

--- flake.nix
+++ flake.nix
@@ -10,54 +10,61 @@
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
 
-
-    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-      name = "flake-nvim-typescript";
+    packages.x86_64-linux.vimrc = pkgs.stdenv.mkDerivation {
+      name = "vimrc";
       src = ./.;
       buildInputs = with pkgs; [
-        nvim-vimrc-code.packages.x86_64-linux.default
+        nvim-vimrc-code.packages.x86_64-linux.vimrc
       ];
       installPhase = ''
-        mkdir -p $out/etc/nvim
+        mkdir -p $out
         cat \
-        ${nvim-vimrc-code.packages.x86_64-linux.default.outPath}/etc/nvim/vimrc \
         ${./lua-start} \
         ${./treesitter.lua} \
         ${./lspconfig.lua} \
-        ${./lua-end}  > $out/etc/nvim/vimrc
+        ${./lua-end}  > $out/vimrc
       '';
     };
 
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = let
-        vimrcPath = "${self.packages.x86_64-linux.default}/etc/nvim/vimrc";
-        local-neovim = pkgs.neovim.override {
-          configure = {
-              # Additional plugins to be installed
-              packages.myVimPackages = with pkgs.vimPlugins; {
-                start = [
-                  vim-nix
-                  copilot-vim
-                  vim-surround
-                  nvim-lspconfig
-                  nvim-cmp
-                  cmp-nvim-lsp
-                  (nvim-treesitter.withPlugins (p: with p; [ python ]))
-                ];
-                opt = [ ];
-              };
-              customRC = builtins.readFile vimrcPath;
-            };
-          };
-      in [
-        pkgs.python311Packages.mypy
-        pkgs.python311Packages.pylint
-        pkgs.python311Packages.python-lsp-server
-        pkgs.python311Packages.pylsp-mypy
-        pkgs.python311
-        local-neovim
-        pkgs.nodejs
-      ];
+    lib = let
+      vimrcPath = "${self.packages.x86_64-linux.vimrc}/vimrc";
+      extraVimrcLines = builtins.readFile vimrcPath;
+    in {
+      version = "1.0.0";
+      neovimForPython = {
+        python ? pkgs.python311,
+        pythonPackages ? pkgs.python311Packages,
+        extraPythonPackages ? [ ],
+        extraVimrcLines ? "",
+        extraVimPlugins ? [ ],
+        extraNixDerivations ? [ ],
+      }: nvim-vimrc-code.lib.neovim {
+        extraVimrcLines = extraVimrcLines;
+        extraVimPlugins = with pkgs.vimPlugins; ( [
+          vim-surround
+          nvim-lspconfig
+          nvim-cmp
+          cmp-nvim-lsp
+          (nvim-treesitter.withPlugins (p: with p; [ python ]))
+        ] ++ extraVimPlugins);
+        extraNixDerivations = [
+          python
+        ] ++ extraNixDerivations ++ (with pythonPackages; ( [
+          mypy
+          pylint
+          python-lsp-server
+          pylsp-mypy
+        ] ++ extraPythonPackages));
+      };
     };
+        
+
+    devShells.x86_64-linux.default = self.lib.neovimForPython { };
+
+    packages.x86_64-linux.default = pkgs.buildEnv {
+      name = "nvim-python";
+      paths = [];
+    };
+
   };
 }
